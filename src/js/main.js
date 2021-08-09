71651b7f3bd2c6062101c1b096adf9d8afcba66a
JuditAldeguer <jud.vicens@hotmail.es>
2021-08-09 13:42:03 +0200

    hidden

--- src/js/main.js
+++ src/js/main.js
@@ -3,33 +3,33 @@
 console.log('>> Hey :)');
 
 
-//1
-const collapsableContent = document.querySelector ('.js_collapsable_content2');
+// //1
+// const collapsableContent = document.querySelector ('.js_collapsable_content');
 
-if (collapsableContent.classList.contains('hidden')) {
-collapsableContent.classList.remove('hidden')
-}
-else {
-collapsableContent.classList.add('hidden')
-}
+// if (collapsableContent.classList.contains('hidden')) {
+// collapsableContent.classList.remove('hidden');
+// }
+// else {
+// collapsableContent.classList.add('hidden');
+// }
 
-//2
+// //2
 
-const collapsableContent2 = document.querySelector ('.js_collapsable_content2');
+// const collapsableContent2 = document.querySelector ('.js_collapsable_content2');
 
-if (collapsableContent2.classList.contains('hidden')) {
-collapsableContent2.classList.remove('hidden')
-}
-else {
-collapsableContent2.classList.add('hidden')
-}
+// if (collapsableContent2.classList.contains('hidden')) {
+// collapsableContent2.classList.remove('hidden');
+// }
+// else {
+// collapsableContent2.classList.add('hidden');
+// }
 
-//3
-const collapsableContent3 = document.querySelector ('.js_collapsable_content3');
+// //3
+// const collapsableContent3 = document.querySelector ('.js_collapsable_content3');
 
-if (collapsableContent3.classList.contains('hidden')) {
-collapsableContent3.classList.remove('hidden')
-}
-else {
-collapsableContent3.classList.add('hidden')
-}
+// if (collapsableContent3.classList.contains('hidden')) {
+// collapsableContent3.classList.remove('hidden');
+// }
+// else {
+// collapsableContent3.classList.add('hidden');
+// }
