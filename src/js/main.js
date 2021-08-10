8f2246358e28100e108aa257d71de24b192ce9ad
JuditAldeguer <jud.vicens@hotmail.es>
2021-08-10 16:57:08 +0200

    animation

--- src/js/main.js
+++ src/js/main.js
@@ -4,28 +4,28 @@ console.log('>> Hey :)');
 
 
 // 1
-//  const collapsableContent = document.querySelector ('.js_collapsable_content');
+const collapsableContent = document.querySelector ('.js_collapsable_content');
 
-//  if (collapsableContent.classList.contains('hidden')) {
-//  collapsableContent.classList.remove('hidden');
-//  }
-//  else {
-//  collapsableContent.classList.add('hidden');
-//  }
+if (collapsableContent.classList.contains('hidden')) {
+collapsableContent.classList.remove('hidden');
+}
+else {
+collapsableContent.classList.add('hidden');
+}
 
 
 // 2
-//  const collapsableContent2 = document.querySelector ('.js_collapsable_content2');
+const collapsableContent2 = document.querySelector ('.js_collapsable_content2');
 
-//  if (collapsableContent2.classList.contains('hidden')) {
-//  collapsableContent2.classList.remove('hidden');
-//  }
-//  else {
-//  collapsableContent2.classList.add('hidden');
-//  }
+if (collapsableContent2.classList.contains('hidden')) {
+collapsableContent2.classList.remove('hidden');
+}
+else {
+collapsableContent2.classList.add('hidden');
+}
 
-//  3
-//  const collapsableContent3 = document.querySelector ('.js_collapsable_content3');
+//3
+//const collapsableContent3 = document.querySelector ('.js_collapsable_content3');
 
 //  if (collapsableContent3.classList.contains('hidden')) {
 //  collapsableContent3.classList.remove('hidden');
