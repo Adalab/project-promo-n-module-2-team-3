991470947200e98fe98b14a4d1fd9a6516a35667
Soraya Alonso <salonsocuesta@gmail.com>
2021-08-11 22:05:29 +0200

    Update docs

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";const fr=new FileReader,fileField=document.querySelector(".js__profile-upload-btn"),profileImage=document.querySelector(".js__profile-image"),profilePreview=document.querySelector(".js__profile-preview");function getImage(e){const r=e.currentTarget.files[0];fr.addEventListener("load",writeImage),fr.readAsDataURL(r)}function writeImage(){profileImage.style.backgroundImage=`url(${fr.result})`,profilePreview.style.backgroundImage=`url(${fr.result})`}function fakeFileClick(){fileField.click()}fileField.addEventListener("change",getImage);
