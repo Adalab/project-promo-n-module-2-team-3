4d287abd38e3a581627cf8039f3a05d6231aa110
sheila <sheila18.sa@gmail.com>
2021-09-02 14:03:23 +0200

    coments

--- src/js/01collapsable.js
+++ src/js/01collapsable.js
@@ -1,22 +1,29 @@
 'use strict'; //acordarnos de borrar
 //Variables
+//En este caso las variables estarán formadas por arrays, por ello usaremos el querySelectorAll
 const collapsableContent = document.querySelectorAll('.js_collapsable_content');
 const collapsableTitle = document.querySelectorAll('.js_collapsable_title');
 console.log(collapsableContent);
 console.log(collapsableTitle);
 
 //Handler
+//Empezaremos crando la función del collapsable
 function collapsable() {
+  //Al haber usado arrays necesitaremos que un bucle ( en este calor for..of) nos recorra el arrays y así poder sacar los elementos de ella 
   for(let collapsableEl of collapsableContent){
+    //Ahora en cada uno de los elementos de estos elementos, le añadiremos/quitaremos la clase deseada
     collapsableEl.classList.toggle('hidden');
   }
 }
+//Esta es la función global la cual usaremos para realizar el evento 
 function handleCollapsable(ev){
   collapsable();
-  console.log(event.currentTarget);
+  console.log(event.currentTarget); //Usaremos el event.currentTarget para verificar que todo funciona
 }
 
+//Volveremos a hacer otro bucle, pero en este caso sacaremos los elementos del titulo
 for(let titleEl of collapsableTitle) {
+  //En estos elementos realizaremos el evento deseado
   titleEl.addEventListener('click', handleCollapsable);
 }
 
