d4bb9a405261d07883151a226cf315167d2c4495
Soraya Alonso <salonsocuesta@gmail.com>
2021-09-02 13:54:28 +0200

    Add changes in js

--- src/js/01collapsable.js
+++ src/js/01collapsable.js
@@ -1,24 +1,22 @@
+'use strict'; //acordarnos de borrar
 //Variables
-const collapsableContent = document.querySelector('.js_collapsable_content');
-const collapsableTitle = document.querySelector('.desing__title');
-const collapsableContent2 = document.querySelector('.js_collapsable_content2');
-const collapsableContent3 = document.querySelector('.js_collapsable_content3');
+const collapsableContent = document.querySelectorAll('.js_collapsable_content');
+const collapsableTitle = document.querySelectorAll('.js_collapsable_title');
+console.log(collapsableContent);
+console.log(collapsableTitle);
 
 //Handler
-function handleCollapsableTarget(event) {
-  console.log('hey');
-  collapsableContent.classList.toggle('hidden');
+function collapsable() {
+  for(let collapsableEl of collapsableContent){
+    collapsableEl.classList.toggle('hidden');
+  }
+}
+function handleCollapsable(ev){
+  collapsable();
+  console.log(event.currentTarget);
 }
 
-//  if (event.currentTarget.) {
-//      collapsableContent.classList.toggle('hidden');
-//      }
-// else if (collapsableContent2.classList.contains('hidden')) {
-//         collapsableContent2.classList.toggle('hidden');
-//     }
-// else if (collapsableContent3.classList.contains('hidden')) {
-//     collapsableContent3.classList.toggle('hidden');
-// }
+for(let titleEl of collapsableTitle) {
+  titleEl.addEventListener('click', handleCollapsable);
+}
 
-//Listener
-collapsableTitle.addEventListener('click', handleCollapsableTarget);
