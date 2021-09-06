02268d6362568af7e06f82eb98e3f238ede38331
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-06 14:35:17 +0200

    update js collapsable

--- src/js/01collapsable.js
+++ src/js/01collapsable.js
@@ -5,68 +5,42 @@ const collapsableContent = document.querySelectorAll('.js_collapsable_content');
 const collapsableTitle = document.querySelectorAll('.js_collapsable_title');
 const arrowAll = document.querySelectorAll('.js_arrow');
 
-
 //Handler
 //Empezaremos crando la función del collapsable
-function collapsable() {
-  //Al haber usado arrays necesitaremos que un bucle ( en este calor for..of) nos recorra el arrays y así poder sacar los elementos de ella
-  for(let collapsableEl of collapsableContent){
-    //Ahora en cada uno de los elementos de estos elementos, le añadiremos/quitaremos la clase deseada
-    collapsableEl.classList.toggle('hidden');
-  }
-}
 
-
-function arrow(){
+function arrow() {
   for (let i = 0; i < arrowAll.length; i++) {
     const arrowEl = arrowAll[i];
   }
 
-  for( const arrowEl of arrowAll ){
-
+  for (const arrowEl of arrowAll) {
     arrowEl.classList.toggle('rotateArrow');
-
   }
-}
-
-/////////////////////////////////////////////////NOS HEMOS QUEDADO CREANDO ESTA FUNCIÓN QUE ES LA DE LOS COLLAPSABLES, Y LO QUE QUEREMOS HACER ES QUE SOLO SE DESPLIEGUE UNO MIENTRAS LOS DEMÁS ESTÁN CERRADOS//////////////////////////
-
-function conditional(event){
-
-  let titleEl = event.currentTarget;
-  console.log(titleEl);
-  titleEl.classList.add('show');
-
-
-
-  for (let i = 0; i < arrowAll.length; i++) {
-    titleEl = collapsableTitle[i];
-
-
-    if(titleEl.classList.contains('hidden') === true ){
-      titleEl.classList.remove('hidden');
-
-
-    }
-
-
-
-  }
-
 }
 
 //Esta es la función global la cual usaremos para realizar el evento
-function handleCollapsable(event){
-
-  collapsable();
+function handleCollapsable(event) {
+  debugger;
+  //Al haber usado arrays necesitaremos que un bucle ( en este calor for..of) nos recorra el arrays y así poder sacar los elementos de ella
+  const titleEl = event.currentTarget;
+  console.log(titleEl);
+  //faltará enlazar los arrays de titulos con los arrays de contenido --------------------------------
+  for (let i = 0; i < collapsableContent.length; i++) {
+    let collapsableContentEl = collapsableContent[i];
+    console.log(collapsableContentEl);
+    if (titleEl.classList.contains('clicado') === true) {
+      collapsableContentEl.classList.toggle('hidden');
+    } else {
+      collapsableContentEl.classList.add('hidden');
+    }
+  }
   arrow();
 
   console.log(event.currentTarget); //Usaremos el event.currentTarget para verificar que todo funciona
 }
 
 //Volveremos a hacer otro bucle, pero en este caso sacaremos los elementos del titulo
-for(let titleEl of collapsableTitle) {
+for (let titleEl of collapsableTitle) {
   //En estos elementos realizaremos el evento deseado
   titleEl.addEventListener('click', handleCollapsable);
 }
-
