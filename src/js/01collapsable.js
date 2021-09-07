1547db60d4abadda987d061f163f0ae4bfc10b04
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-07 13:18:14 +0200

    update arrows JS

--- src/js/01collapsable.js
+++ src/js/01collapsable.js
@@ -9,42 +9,30 @@ const arrowAll = document.querySelectorAll('.js_arrow');
 
 //Handler
 
-function arrow() {
-  for (let i = 0; i < arrowAll.length; i++) {
-    const arrowEl = arrowAll[i];
-  }
-
-  for (const arrowEl of arrowAll) {
-    arrowEl.classList.toggle('rotateArrow');
-  }
-}
-
 //Esta es la función global la cual usaremos para realizar el evento
 function handleCollapsable(event) {
   //Al haber usado arrays necesitaremos que un bucle ( en este calor for..of) nos recorra el arrays y así poder sacar los elementos de ella
-  debugger;
   const titleEl = event.currentTarget;
   console.log(titleEl);
   titleEl.classList.add('clicado');
   //ustedes tenian la lógica que el titulo al ser clicado se le añadiera una clase, lo deje asi
   //en la siguiente variable seleccione todos los titulos para saber cual es el nuevo que tiene la clace "clicado"
-
-  //faltará enlazar los arrays de titulos con los arrays de contenido --------------------------------
   for (let i = 0; i < collapsableContentAll.length; i++) {
     let collapsableContentEl = collapsableContentAll[i];
     //aqui debemos preguntar si el titulo esta clicado, pero los titulos al ser una lista se debe acceder
     //a cada posicion con [i]
     if (collapsableTitleList[i].classList.contains('clicado') === true) {
       collapsableContentEl.classList.toggle('hidden');
+      arrowAll[i].classList.toggle('rotateArrowUp');
       //debemos borrarle la clase clicado porque cada iteraccion que haga el usuario solo debe haber un clicado
       collapsableTitleList[i].classList.remove('clicado');
     } else {
       //aqui tambien debemos quitar la clase clicado, porque solo puede haber uno a la vez
       collapsableTitleList[i].classList.remove('clicado');
       collapsableContentEl.classList.add('hidden');
+      arrowAll[i].classList.remove('rotateArrowUp');
     }
   }
-  arrow();
   console.log(event.currentTarget); //Usaremos el event.currentTarget para verificar que todo funciona
 }
 
