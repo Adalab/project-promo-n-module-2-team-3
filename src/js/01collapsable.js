87b8cf2eebf7b6bfa63d1907d54a418af11af4c1
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-07 12:11:42 +0200

    add collapsable function with Dayana's help

--- src/js/01collapsable.js
+++ src/js/01collapsable.js
@@ -2,7 +2,7 @@
 //Variables
 //En este caso las variables estarán formadas por arrays, por ello usaremos el querySelectorAll
 const collapsableContent = document.querySelectorAll('.js_collapsable_content');
-const collapsableTitle = document.querySelectorAll('.js_collapsable_title');
+const collapsableTitleList = document.querySelectorAll('.js_collapsable_title');
 const arrowAll = document.querySelectorAll('.js_arrow');
 
 //Handler
@@ -20,22 +20,30 @@ function arrow() {
 
 //Esta es la función global la cual usaremos para realizar el evento
 function handleCollapsable(event) {
-  debugger;
   //Al haber usado arrays necesitaremos que un bucle ( en este calor for..of) nos recorra el arrays y así poder sacar los elementos de ella
+  debugger;
   const titleEl = event.currentTarget;
   console.log(titleEl);
+  titleEl.classList.add('clicado');
+  //ustedes tenian la lógica que el titulo al ser clicado se le añadiera una clase, lo deje asi
+  //en la siguiente variable seleccione todos los titulos para saber cual es el nuevo que tiene la clace "clicado"
+
   //faltará enlazar los arrays de titulos con los arrays de contenido --------------------------------
   for (let i = 0; i < collapsableContent.length; i++) {
     let collapsableContentEl = collapsableContent[i];
-    console.log(collapsableContentEl);
-    if (titleEl.classList.contains('clicado') === true) {
+    //aqui debemos preguntar si el titulo esta clicado, pero los titulos al ser una lista se debe acceder
+    //a cada posicion con [i]
+    if (collapsableTitleList[i].classList.contains('clicado') === true) {
       collapsableContentEl.classList.toggle('hidden');
+      //debemos borrarle la clase clicado porque cada iteraccion que haga el usuario solo debe haber un clicado
+      collapsableTitleList[i].classList.remove('clicado');
     } else {
+      //aqui tambien debemos quitar la clase clicado, porque solo puede haber uno a la vez
+      collapsableTitleList[i].classList.remove('clicado');
       collapsableContentEl.classList.add('hidden');
     }
   }
   arrow();
-
   console.log(event.currentTarget); //Usaremos el event.currentTarget para verificar que todo funciona
 }
 
